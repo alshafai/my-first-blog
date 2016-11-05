17a52fcac4f45089496151d71137391cf4c33234
ali <saihat1422@gmail.com>
2016-11-04 21:23:02 -0700

    Django Fourth

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$eqog!xuw0fn_xv)=)_5kn8k9-t!7$493$(t)-taa&!3bmwtj_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alshafai.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
